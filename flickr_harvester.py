3a56ab299b959ad83142c8b38c726c327d085350
Justin Littman <justinlittman@gwu.edu>
2016-09-19 11:14:38 -0400

    refs sfm 446. Control warcprox logging.

--- flickr_harvester.py
+++ flickr_harvester.py
@@ -11,8 +11,8 @@ ROUTING_KEY = "harvest.start.flickr.*"
 
 
 class FlickrHarvester(BaseHarvester):
-    def __init__(self, working_path, mq_config=None, debug=False, per_page=None):
-        BaseHarvester.__init__(self, working_path, mq_config=mq_config, debug=debug)
+    def __init__(self, working_path, mq_config=None, debug=False, per_page=None, debug_warcprox=False):
+        BaseHarvester.__init__(self, working_path, mq_config=mq_config, debug=debug, debug_warcprox=debug_warcprox)
         self.api = None
         # For testing purposes
         self.per_page = per_page
