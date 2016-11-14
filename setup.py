158423d7f0a1ec33f878f0534a4a139e500a0578
Justin Littman <justinlittman@gwu.edu>
2016-11-14 14:08:20 -0500

    Bumped version to 1.3.0 and set sfm-utils version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='sfmflickrharvester',
-    version='1.2.0',
+    version='1.3.0',
     url='https://github.com/gwu-libraries/sfm-flickr-harvester',
     author='Justin Littman',
     author_email='justinlittman@gmail.com',
