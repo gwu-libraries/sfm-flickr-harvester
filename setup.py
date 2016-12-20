bdfdab7fb24a0b4b097b6e5da5483bd48889fd97
Justin Littman <justinlittman@gwu.edu>
2016-12-20 14:43:16 -0500

    Updated version to 1.4.0.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='sfmflickrharvester',
-    version='1.3.1',
+    version='1.4.0',
     url='https://github.com/gwu-libraries/sfm-flickr-harvester',
     author='Justin Littman',
     author_email='justinlittman@gmail.com',
@@ -12,7 +12,7 @@ setup(
     scripts=['flickr_harvester.py',
              'flickr_warc_iter.py'],
     install_requires=['sfmutils'],
-    tests_require=['mock>=1.3.1'],
+    tests_require=['mock>=1.4.0'],
     classifiers=[
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Libraries :: Python Modules',
