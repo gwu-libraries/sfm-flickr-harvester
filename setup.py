7da9f5143461ef9a0c40a379c551c176d38dccea
Justin Littman <justinlittman@gwu.edu>
2017-08-28 10:09:12 -0400

    Bumped version to 1.10.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='sfmflickrharvester',
-    version='1.9.0',
+    version='1.10.0',
     url='https://github.com/gwu-libraries/sfm-flickr-harvester',
     author='Justin Littman',
     author_email='justinlittman@gmail.com',
